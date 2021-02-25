b5b7e8abaad7ff5d366e4da5386a088fb13008dd
Karan Rajpal <rajpal.karan73@gmail.com>
2021-02-24 21:55:09 -0600

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import Products from "./components/Products/Products";
+
+import { Products, Navbar } from "./components";
 
 class App extends Component {
 	render() {
 		return (
 			<div>
+				<Navbar />
 				<Products />
 			</div>
 		);
